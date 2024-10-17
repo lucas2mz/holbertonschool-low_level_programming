106845142f0b0e1e269c6fc9f1f90fa596658a26
lucas2mz <lucasandrada2606@gmail.com>
2024-10-17 18:01:09 +0000

    Task 6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the size of various data types
+ * in bytes using the sizeof operator.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
