0637c73c382e0c388d7abfec8bb2ed91ec1880f7
lucas2mz <lucasandrada2606@gmail.com>
2024-10-17 18:04:51 +0000

    Task 6

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
-int main (void) {
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the size of various data types
+ * in bytes using the sizeof operator.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-
-	return 0;
-		
 }
