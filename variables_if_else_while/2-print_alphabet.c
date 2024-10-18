3ea96318ffcaa1d723cc78d21f5cbded5c50c7bc
lucas2mz <lucasandrada2606@gmail.com>
2024-10-18 17:12:33 +0000

    Task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	
+	int i, a;
+
+	a = 97;
+	char alph = a;
+
+	while (a < 123)
+{
+	putchar(alph);
+	a = a + 1;
+	alph = alph + 1;
+}
+	return (0);
 }
