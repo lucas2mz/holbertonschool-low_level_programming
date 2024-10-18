a6fb904412a5b5a6d02111e722319f223f15b07c
root <lucasandrada2606@gmail.com>
2024-10-18 21:17:47 +0000

    Test

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the alphabet
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+{
+	if (alph = 'e', 'q')
+{
+	alph = alph + 1;
+}
+	else	
+{
+	putchar(alph);
+	alph = alph + 1;
+}
+}
+	putchar('\n');
+	return (0);
+}
