9f19824c0e628ca86c0799ddcf37c2b8a35935b4
lucas2mz <lucasandrada2606@gmail.com>
2024-10-21 12:41:10 +0000

    Task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * This function prints numbers of base 10
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 
 	i = 0;
 
-	while(i < 10)
+	while (i < 10)
 {
-	printf("%d",i);
+	printf("%d", i);
 	i++;
 }
 	printf("\n");
