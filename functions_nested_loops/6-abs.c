a90b13096b022d07f834e6ac5771a6132cf50e38
lucas2mz <lucasandrada2606@gmail.com>
2024-10-23 14:46:05 +0000

    Task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * _abs - Entry point of the program
+ *
+ * _abs - checks if the int iss - or +
+ * and returns his abs
+ *
+ * @i: the character to be checked
+ *
+ * Return: i if @i is a +, -i if @i is -
+ *
+ */
 int _abs(int i)
 {
 	if (i < 0)
