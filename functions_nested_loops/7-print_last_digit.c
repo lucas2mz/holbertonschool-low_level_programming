5897a946866f92e270b4e3aad588715bd5be23e9
lucas2mz <lucasandrada2606@gmail.com>
2024-10-23 15:07:21 +0000

    Task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
  * print_last_digit - Entry point of the program
  *
@@ -15,5 +18,7 @@ int print_last_digit(int d)
 
 	ld = d % 10;
 
+	char last = ld + '0';
+	write(1, %last, 1);
 	return (ld);
 }
