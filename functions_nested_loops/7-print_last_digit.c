01843fc9ec72166005f557f4ed41656084fb8222
lucas2mz <lucasandrada2606@gmail.com>
2024-10-23 14:57:56 +0000

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point of the program
+ *
+ * print_last_digit - return the last digit of an int
+ *
+ * @d: the character to be checked
+ *
+ * Return: ld
+ *
+ */
+int print_last_digit(int d)
+{
+	int ld;
+
+	ld = d % 10;
+
+	return (ld);
+}
