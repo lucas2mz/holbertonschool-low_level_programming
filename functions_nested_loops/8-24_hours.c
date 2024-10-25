484fae854bc03cf658e2e452d2accf94afef3f2e
lucas2mz <lucasandrada2606@gmail.com>
2024-10-25 16:10:37 +0000

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,20 +1,25 @@
 #include "main.h"
-
+/**
+ * jack_bauer - Entry point of the program
+ *
+ * jack_bauer - return the hour
+ *
+ */
 void jack_bauer(void)
 {
 	int hora1, hora2, minuto1, minuto2;
 
-	for (hora1 = 0;hora1 < 3;hora1++)
+	for (hora1 = 0; hora1 < 3; hora1++)
 	{
-	for (hora2 = 0;hora2 < 10;hora2++)
+	for (hora2 = 0; hora2 < 10; hora2++)
 	{
 	if (hora1 == 2 && hora2 > 3)
 {
 	break;
 }
-	for (minuto1 = 0;minuto1 < 6;minuto1++)
+	for (minuto1 = 0; minuto1 < 6; minuto1++)
 	{
-	for (minuto2 = 0;minuto2 < 10;minuto2++)
+	for (minuto2 = 0; minuto2 < 10; minuto2++)
 	{
 	_putchar('0' + hora1);
 	_putchar('0' + hora2);
