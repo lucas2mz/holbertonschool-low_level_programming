1ac128d7d4fb12bc7bc04db03c23e6bc36b573e1
lucas2mz <lucasandrada2606@gmail.com>
2024-10-26 22:14:23 +0000

    Task 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,13 @@
 #include "main.h"
-
+/**
+ * add - Adds two integers
+ * @x: the first integer to add
+ * @y: the second integer to add
+ *
+ * This function takes two integers and returns their sum.
+ *
+ * Return: The sum of x and y
+ */
 int add(int x, int y)
 {
 	int resultado;
