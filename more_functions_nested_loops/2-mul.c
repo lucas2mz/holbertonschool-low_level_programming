dccb00511d856e141a5199765ec6efde2045ca5a
lucas2mz <lucasandrada2606@gmail.com>
2024-10-28 13:32:56 +0000

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Entry of the program
+ *
+ * mul - Multiplies two integers
+ *
+ * Return: resultado*/
+int mul(int a, int b)
+{
+	int resultado;
+
+	resultado = (a * b);
+
+	return (resultado);
+}
