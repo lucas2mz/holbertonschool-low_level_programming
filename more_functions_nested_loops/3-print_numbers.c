8b96493083a6cb626a0c1623e4e60b67b0f184cc
lucas2mz <lucasandrada2606@gmail.com>
2024-10-28 13:46:35 +0000

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Entry the program
+ *
+ * print_numbers - Print Numbers
+ *
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	_putchar(c);
+}
+	_putchar('\n');
+}
