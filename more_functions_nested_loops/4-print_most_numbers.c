c6142d58d55f54fc55919360a2ef975c4be9280b
lucas2mz <lucasandrada2606@gmail.com>
2024-10-28 14:00:16 +0000

    Task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry the program
+ *
+ * print_most_numbers - Print Numbers
+ *
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	if (c != 50 && c != 52)
+{
+	_putchar(c);
+}
+}
+	_putchar('\n');
+}
