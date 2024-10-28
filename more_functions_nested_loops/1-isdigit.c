21834dd781807cdb0095b884f28b09223f08b446
lucas2mz <lucasandrada2606@gmail.com>
2024-10-28 13:11:48 +0000

    Task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isdigit - Entry of the program
+ *
+ * _isdigit - Checks for a digit
+ *
+ * @c: the var to be checked
+ *
+ * Return: 1 if is a digit otherwise return 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
