118a21bc995e5a3200330ab303736cff3eead957
lucas2mz <lucasandrada2606@gmail.com>
2024-10-29 17:33:30 +0000

    task 7

---
+++ more_functions_nested_loops/7-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_diagonal(5);
+    return (0);
+}
---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+void print_diagonal(int n)
+{
+	int i, x;
+
+	if (n > 0)
+{
+	for (i = 1; i <= n; i++)
+{
+	_putchar('\\');
+	_putchar('\n');
+	for (x = 1; x <= i; x++)
+{
+	_putchar(' ');
+}
+}
+}
+	else
+{
+	_putchar('\n');
+}
+}
