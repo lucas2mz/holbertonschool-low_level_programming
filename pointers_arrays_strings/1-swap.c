d1064591cf2e4a6379258070c1e28ed5d2970607
lucas2mz <lucasandrada2606@gmail.com>
2024-10-30 14:36:01 +0000

    Task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - Entry of the program
+ * swap_int - Swap integers
+ * @a: var to be checked
+ * @b: var to be checked
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
