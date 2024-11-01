fd614d222220253b92b13d28fb7000e0fd212df6
lucas2mz <lucasandrada2606@gmail.com>
2024-11-01 12:35:18 +0000

    task9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,24 @@
 #include "main.h"
-
+/**
+ * _strcpy - Entry of the program
+ * _strcpy - Copy a string
+ * @dest: var to be checked
+ * @src: var to be checked
+ * Return: dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i, len;
 
+	len = 0;
+
 	while (src[len] != '\0')
 {
-			len++;
+	len++;
 }
-	for (i = 0; )
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
