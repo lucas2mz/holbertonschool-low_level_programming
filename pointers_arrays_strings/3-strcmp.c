ac6b7292780170d73293f9ad9051511804ec2e01
lucas2mz <lucasandrada2606@gmail.com>
2024-11-02 20:36:32 +0000

    Task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcmp - Entry of the program
+ * _strcmp - Compares Strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 15 if is positive, -15 if is negative, 0 if is equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+		}
+
+		if (i > j)
+		{
+			return (15);
+		}
+		else if (i < j)
+		{
+			return (-15);
+		}
+		else
+		return (0);
+}
