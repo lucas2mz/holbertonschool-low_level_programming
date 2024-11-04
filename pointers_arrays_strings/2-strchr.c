b320ad60857b7d9e633f08ac7305c10f5e7c1e79
lucas2mz <lucasandrada2606@gmail.com>
2024-11-04 15:08:31 +0000

    Task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - Entry of the program
+ * _strchr - asjfnjkdf
+ * @s: Array to be checked
+ * @c: var to be checked
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
