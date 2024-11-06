499a27d8351d023d4866d0a640a4ab80b051fe3c
lucas2mz <lucasandrada2606@gmail.com>
2024-11-06 16:43:45 +0000

    Task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
+/**
+ * _strlen_recursion - Entry of the program
+ * _strlen_recursion - Prints the len of a string
+ * @s: String to be checked
+ * Return: _strlen_recursion
+ */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		return (_strlen_recursion(s + 1));
+		return (0);
 	}
-	else
-	{
-		_putchar('\n');
-	}
-	return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
