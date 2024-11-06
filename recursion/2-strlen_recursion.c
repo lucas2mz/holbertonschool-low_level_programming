5168d4b1db35044cabbb308cd89eb3d74d2bba4f
lucas2mz <lucasandrada2606@gmail.com>
2024-11-06 16:03:46 +0000

    Task 1

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,18 +2,13 @@
 
 int _strlen_recursion(char *s)
 {
-	int n;
-
-	n = 0;
-
 	if (*s != '\0')
 	{
-		n++;
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
 		_putchar('\n');
 	}
-	return (n);
+	return (0);
 }
