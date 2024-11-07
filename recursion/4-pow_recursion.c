1090af24471a43bd509deb1fd310999eafebeb5d
lucas2mz <lucasandrada2606@gmail.com>
2024-11-07 12:32:04 +0000

    Task 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _pow_recursion - Entry of the program
+ * _pow_recursion - ndsjklsd
+ * @x: var to be checked
+ * @y: var to be checked
+ * Return: 1 or -1 or the function
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
