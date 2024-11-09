e77c88df3dde7847613bc72aa586db1445e613e4
lucas2mz <lucasandrada2606@gmail.com>
2024-11-09 20:53:48 +0000

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * @argc: jsdfjkbndfg
+ * @argv: klasdfhnjkl
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
