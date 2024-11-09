ca3daf435e132c94e612ca8a6e3ace14430544b4
lucas2mz <lucasandrada2606@gmail.com>
2024-11-09 21:01:47 +0000

    Task 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Prints each argument passed to the program, one per line
+ * @argc: The number of arguments
+ * @argv: An array of strings representing the arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	int i;
