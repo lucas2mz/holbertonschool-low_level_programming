35a47047b61daa365c74f1bbcf65f8e80b9abd00
lucas2mz <lucasandrada2606@gmail.com>
2024-11-10 23:54:20 +0000

    Task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,3 +2,28 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+		if (argv[i][j] < '0' || argv[i][j] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		}
+		sum += atoi(argv[i]);
+	}
+		if (argc > 1)
+		{
+		printf("%d\n", sum);
+		} 
+		else
+		{
+		printf("0\n");
+		}
+	return (0);
+}
