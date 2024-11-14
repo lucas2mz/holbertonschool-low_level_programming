96e1bada76fb8032deb694ba9b59300356a14e1f
lucas2mz <lucasandrada2606@gmail.com>
2024-11-14 15:17:52 +0000

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure to represent a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
