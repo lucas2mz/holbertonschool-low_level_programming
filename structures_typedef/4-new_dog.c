a2cca67d929370de1de11076144b7e785787b3f7
lucas2mz <lucasandrada2606@gmail.com>
2024-11-16 23:45:07 +0000

    Task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - Entry of the program
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: NULL or aux
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{ 
+	dog_t *aux = malloc(sizeof(dog_t));
+
+	if (aux == NULL)
+	{
+		return (NULL);
+	}
+
+	aux->name = name;
+	aux->age = age;
+	aux->owner = owner;
+
+	return (aux);
+}
