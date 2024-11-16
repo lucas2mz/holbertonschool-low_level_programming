7c910d48388ba48ba919cda4bf313dfb39d344c6
lucas2mz <lucasandrada2606@gmail.com>
2024-11-16 23:15:58 +0000

    Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,6 +6,12 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		printf("Name: (nil)");
+		printf("Age: (nil)");
+		printf("Owner: (nil)");
+	}
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)");
