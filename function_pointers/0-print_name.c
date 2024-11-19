a031e5389a1d1f00c4f9d0dccfa35f4c94af638c
lucas2mz <lucasandrada2606@gmail.com>
2024-11-19 19:00:15 +0000

    Task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+ * print_name - Print name
+ * @name: Name to print
+ * @f: pointer to a function
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	(*f)(name);
