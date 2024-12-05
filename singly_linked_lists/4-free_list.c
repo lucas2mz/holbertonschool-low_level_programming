ec59c3312a561fc93cf7914f6360aefdac5a9d82
lucas2mz <lucasandrada2606@gmail.com>
2024-12-05 14:45:20 +0000

    Task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - Free the list
+ * @head: Header of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
