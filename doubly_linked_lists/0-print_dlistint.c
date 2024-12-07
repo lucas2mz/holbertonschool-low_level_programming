8399d76a9470e8809e9670403bbc69b65b22fd75
lucas2mz <lucasandrada2606@gmail.com>
2024-12-07 20:47:54 +0000

    Task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,19 @@
 #include "lists.h"
+/**
+ * print_dlistint - Prints the list
+ * @h: The header of the list
+ * Return: The amount of nodes in the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	const dlistint_t *aux = h;
+	size_t i = 0;
+
+	while (aux != NULL)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+		i++;
+	}
+	return (i);
 }
